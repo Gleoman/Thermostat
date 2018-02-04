9b01a3e46afd4d7250c00b42d2d2b3bfe75a19f1
Ed Goold <teddy_goold@hotmail.com>
2018-02-04 14:41:08 +0000

    travis/karma 4

--- jasmine/karma.conf.js
+++ jasmine/karma.conf.js
@@ -56,8 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [
-  ],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
@@ -69,20 +68,3 @@ module.exports = function(config) {
     concurrency: Infinity
   })
 }
-
-var configuration = {
-    // other things
-
-    customLaunchers: {
-        Chrome_travis_ci: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
-        }
-    },
-};
-
-if (process.env.TRAVIS) {
-    configuration.browsers = ['Chrome_travis_ci'];
-}
-
-config.set(configuration);
